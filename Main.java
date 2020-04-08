234ed51c147f955b10badee26d9cd2749c6513aa
Timur Berezov <berezov.timur@gmail.com>
2020-04-08 18:05:56 +0300

    Change README.md

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        float TicketPrice = 1000F;
-        float BonusMile = 20F;
-        float TotalBonus = TicketPrice / BonusMile ;
-        int TotalBonus1 = (int) TotalBonus;
-        System.out.println(TotalBonus1);
+        float ticketprice = 1000F;
+        float bonusmile = 20F;
+        float totalbonus = ticketprice / bonusmile ;
+        int finalbonus = (int) totalbonus;
+        System.out.println(finalbonus);
     }
 }
